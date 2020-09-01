12ded2e5c0812382eac5502b679fba55e07a37fe
ristoxxx <lahteri@gmail.com>
2020-09-01 17:10:40 +0300

    eka

---
+++ helloworld/src/main/java/com/example/helloworld/web/helloController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class helloController {
+	@RequestMapping("*")
+	public String hello() {
+		return "Hello Spring";
+	}
+
+}
